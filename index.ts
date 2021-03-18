c8de71c3567631c3f4d174426e394ab92bfb0871
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-03-18 16:19:01 +0100

    Propagate license header to all source files

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /*
- * Okaeri Services SDK (Node)
- * Copyright (C) 2021 Okaeri
+ * Okaeri SDK (Node)
+ * Copyright (C) 2021 Okaeri, Dawid Sawicki
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -14,7 +14,6 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 export {AiCensor} from "./src/aicensor";
